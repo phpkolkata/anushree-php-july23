22e3ff98ac98f3a7c5ea4fa53a1e04e0d0b55228
Md Asif Iqbal <phpkolkata@gmail.com>
2023-11-03 08:38:45 +0530

    hw

--- php/22.php
+++ php/22.php
@@ -1,7 +1,46 @@
-<form action="23.php" method="get">
-    Name:<input type="text" name="nm"><br>
-    Age:<input type="number" name="age"><br>
-    <button>Submit</button>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script lang="javascript">
+
+    function check(){
+        var nm = document.getElementById("nm");
+        var age = document.getElementById("age");
+
+
+        // alert(nm.value);
+        if(nm.value == ""){
+            alert("please enter name");
+            return false;
+        }
+        else if(age.value <= 0){
+            alert("please enter age");
+            return false;
+        }
+
+        return true;
+    }
+
+    </script>
+</head>
+<body>
+    <form action="23.php" method="get">
+    Name:<input type="text" name="nm" id="nm"><br>
+    Age:<input type="number" name="age" id="age"><br>
+    Gender:
+    <input type="radio" name="gen" value="male">Male
+    <input type="radio" name="gen" value="female">Female
+
+
+    <button onclick="return check()">Submit</button>
 </form>
+
+</body>
+</html>
+
 
 <!-- when we use get method, data passes through url using  ?nm=raj&age=33&... -->
--- php/23.php
+++ php/23.php
@@ -1,10 +1,12 @@
 <?php
 
-// print_r($_GET);
+print "<pre>";
+print_r($_GET);
 $nm = $_GET['nm'];
 $age = $_GET['age'];
+$gen = $_GET['gen'];
 
-echo "your name is $nm and your age is $age";
+echo "your name is $nm and your age is $age, your age is $gen";
 
 // hw
 /*
